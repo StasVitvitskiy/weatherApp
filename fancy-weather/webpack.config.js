ab145738c7d3f16d2db14642f839c91d0975f6ee
Stanislav Vitvitskiy <stanislavvitvitskiy@Stanislavs-MacBook-Pro.local>
2020-05-30 20:20:07 -0700

    feat: add netlify redirect

--- fancy-weather/webpack.config.js
+++ fancy-weather/webpack.config.js
@@ -2,6 +2,7 @@
 const path = require('path');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.jsx',
@@ -52,6 +53,11 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './index.html',
             base: '/',
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/_redirects', to: '.' },
+            ]
         }),
     ],
     mode: 'development',
